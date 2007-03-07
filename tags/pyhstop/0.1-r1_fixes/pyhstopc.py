4eddb30a083e843868e6e478e2ec8dce1c10663a
flx <flx@b2fd3293-f12a-0410-953c-97324cacd33b>
2007-03-07 15:11:24 +0000

    tagged

---
+++ tags/pyhstop/0.1-r1_fixes/pyhstopc.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+############################################################################
+#    Copyright (C) 2007 by Felix Bechstein   #
+#    flx@un1337.de   #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+import socket
+import Queue
+import threading
+from optparse import OptionParser
+import sys
+import urllib
+import urllib2
+import time
+import md5
+
+QUEUE_TIMEOUT = 3
+DEFAULT_PORT = 9099
+REQUEST_BUFF_SIZE = 128
+SPLITCHAR = '-'
+
+def myHash(input):
+	out = ''
+	m = md5.new(str(input))
+	out = m.hexdigest()
+	return out[0:8]
+
+class socketListener:
+	p = DEFAULT_PORT
+	t = 'tcp'
+	s = None
+	#tin = None
+	tout = None
+	work = True
+	isData = True
+	killSock = False
+	q = None
+	tc = None
+	c = False
+	
+	
+	def __init__(self, tc, sPort, queues, sType = 'tcp'):
+		self.t = sType
+		self.p = sPort
+		self.q = queues
+		self.tc = tc
+		tc.setSL(self)
+		#if self.t =='tcp':
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#else:
+		#	self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	
+	def send(self, c):
+		while self.isData:
+			try:
+				item = self.q.qout.get(True, QUEUE_TIMEOUT)
+				c.send(item)
+			except Queue.Empty:
+				item = None
+		print 'break snd'
+	
+	def do_listen(self):
+		print 'listen.. port=', self.p
+		self.s.bind(('', self.p))
+		while self.work:
+			self.s.listen(1)
+			conn, addr = self.s.accept()
+			print 'Connected by', addr
+			self.q = queues()
+			self.tc.newSID()
+			self.c = True
+			tout = threading.Thread(target=self.send,args=(conn,))
+			tout.setDaemon(True)
+			tout.start()
+			if self.work: self.isData = True
+			self.killSock = False
+			while self.isData and not self.killSock:
+				data = conn.recv(REQUEST_BUFF_SIZE)
+				if not data:
+					self.isData = False
+					print 'break rcv'
+					break
+				print 'snd: ', data.strip()
+				try:
+					self.q.qin.put(data)
+					#conn.send(data)
+				except Queue.Full:
+					data = None
+			self.c = False
+			conn.close()
+	
+	def listen(self):
+		thr = threading.Thread(target=self.do_listen)
+		thr.setDaemon(True)
+		thr.start()
+	
+	def terminate(self):
+		self.work = False
+		self.isData = False
+		print 'socketlistener terminated'
+	
+
+class tunnelClient:
+	q = None
+	url = ''
+	destHost = ''
+	destPort = 0
+	proxy = ''
+	work = True
+	sid = ''
+	sl = None
+	
+	def __init__(self, queues, url, host, port, type, proxy):
+		self.q = queues
+		self.url = url
+		self.destHost = host
+		self.destPort = port
+		self.destType = type
+		self.proxy = proxy
+		self.sid = ''
+	
+	def newSID(self):
+		self.sid = myHash(time.time())
+	
+	def setSL(self, sListener):
+		self.sl = sListener
+	
+	def pushData(self):
+		while self.work:
+			try:
+				item = self.q.qin.get(True, QUEUE_TIMEOUT)
+			except (Queue.Empty, ):
+				item = None
+			if not self.sl.c: continue
+			if self.sid == '': continue
+			m = myHash(time.time())
+			datalist = [('i', self.sid), ('t', self.destType), ('h', self.destHost), ('p', self.destPort), ('b', m)]
+			if item:
+				#try:
+					datalist.append(('d', item))
+				#except AttributeError:
+				#	item = None
+			myurl = self.url + '?' + urllib.urlencode(datalist)
+			try:
+				req = urllib2.Request(url=myurl,)
+				f = urllib2.urlopen(req)
+				ret = f.read()
+				if ret and ret != '':
+					print 'rcv: ', ret.strip()
+					self.q.qout.put(ret)
+			except urllib2.HTTPError:
+				self.sl.killSock = True
+			
+	def connect(self):
+		thr = threading.Thread(target=self.pushData)
+		thr.setDaemon(True)
+		thr.start()
+
+class queues:
+	qin = Queue.Queue()
+	qout = Queue.Queue()
+
+def main():
+	usage = "usage: %prog [options]"
+	parser = OptionParser(usage=usage)
+	parser.add_option('-t', '--tcp', action='store_const', dest='t', const='tcp', default='tcp', help='tcp mode (default)')
+	#parser.add_option('-u', '--udp', action='store_const', dest='t', const='udp', help='udp mode')
+	#	parser.add_option('-q', '--quiet', action="store_const", const=0, dest="v", default=1, help='quiet')
+	#	parser.add_option('-v', '--verbose', action="store_const", const=1, dest="v", help='verbose')
+	
+	parser.add_option('-p', '--port', action="store", type='int', dest="port", help='port to listen', default=DEFAULT_PORT)
+	parser.add_option('--url', action="store", dest="url", help='URL of tunnelendpoint', default='https://localhost:8080/')
+	
+	parser.add_option('-d', '--dest', action="store", dest="dest", help='destination to connect to', default='localhost:9091')
+	#parser.add_option('--proxy', action='store', dest='proxy', help='proxy to use')
+		
+	(options, args) = parser.parse_args()
+	
+	print 'start..'
+	
+	q = queues()
+	
+			
+	tc = tunnelClient(q, options.url, options.dest.split(':')[0], options.dest.split(':')[1], options.t, '')
+	
+	sl = socketListener(tc, options.port,q, sType=options.t)
+	sl.listen()
+		
+	tc.connect()
+	
+	sys.stdin.readline()
+	sl.terminate()
+	print 'end..'
+
+main()
---
+++ tags/pyhstop/0.1-r1_fixes/pyhstopd.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+############################################################################
+#    Copyright (C) 2007 by Felix Bechstein   #
+#    flx@un1337.de   #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+import socket
+import os
+from SocketServer import BaseServer
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import Queue
+import threading
+from optparse import OptionParser
+import sys
+import urllib
+import urllib2
+import time
+import md5
+import cgi
+
+QUEUE_TIMEOUT = 3
+CONECTION_TIMEOUT = QUEUE_TIMEOUT * 2
+DEFAULT_PORT = 9099
+REQUEST_BUFF_SIZE = 128
+SPLITCHAR = '-'
+
+keyfile = ''
+certfile = ''
+
+class queues:
+	qin = Queue.Queue()
+	qout = Queue.Queue()
+
+class sessionItem:
+	q = queues
+	sid = ''
+	t = 'tcp'
+	p = 80
+	h = 'localhost'
+	tin = None
+	tout = None
+	work = True
+	sock = None
+	last = True
+	
+	def tick(self):
+		self.last = True
+	
+	def inThr(self):
+		while self.work:
+			data = self.sock.recv(REQUEST_BUFF_SIZE)
+			if data:
+				self.q.qin.put(data)
+			else:
+				self.q.qin.put(None)
+				self.terminate()
+	
+	def outThr(self):
+		while self.work:
+			try:
+				data = self.q.qout.get(True, QUEUE_TIMEOUT)
+			except Queue.Empty:
+				data = None
+			if data:
+				self.sock.send(data)
+	
+	def terminate(self):
+		if self.work:
+			print 'terminating session: ', self.sid
+		self.work = False
+		self.sock.close()
+	
+	def start(self):
+		try:
+			self.sock.connect((self.h, self.p))
+			self.tin.start()
+			self.tout.start()
+		except socket.error:
+			print 'socketerror for session:', self.sid
+			self.terminate
+			self.work = False
+		
+		
+	def __init__(self, sessionID, socketType, host, port):
+		self.sid = sessionID
+		self.t = socketType
+		self.p = int(port)
+		self.h = host
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.tin = threading.Thread(target=self.inThr)
+		self.tout = threading.Thread(target=self.outThr)
+		self.tin.setDaemon(True)
+		self.tout.setDaemon(True)
+		print 'added session: ', self.sid, self.t, self.h, self.p
+
+class sessionList:
+	l = {};
+	
+	def __init__(self):
+		thr = threading.Thread(target=self.check)
+		thr.setDaemon(True)
+		thr.start()
+	
+	def add(self,sessionID, socketType, host, port):
+		if not self.l.has_key(sessionID):
+			self.l[sessionID] = sessionItem(sessionID, socketType, host, port)
+			self.l[sessionID].start()
+	
+	def rm(self,sessionID):
+		del self.l[sessionID]
+	
+	def get(self,sessionID):
+		try:
+			i = self.l[sessionID]
+			i.tick()
+			return i
+		except KeyError:
+			return None
+	
+	def check(self):
+		while True:
+			time.sleep(CONECTION_TIMEOUT)
+			for i in self.l:
+				if self.l[i].last:
+					self.l[i].last = False
+				else:
+					self.l[i].terminate()
+
+sessionlist = sessionList()
+
+class SecureHTTPServer(HTTPServer):
+    def __init__(self, server_address, HandlerClass):
+	from OpenSSL import SSL
+        BaseServer.__init__(self, server_address, HandlerClass)
+        ctx = SSL.Context(SSL.SSLv23_METHOD)
+        #server.pem's location (containing the server private key and
+        #the server certificate).
+	ctx.use_privatekey_file (keyfile)
+        ctx.use_certificate_file(certfile)
+        self.socket = SSL.Connection(ctx, socket.socket(self.address_family, self.socket_type))
+        self.server_bind()
+        self.server_activate()
+
+
+class myHTTPRequestHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			(stuff, args) = self.path.split('?',1)
+		except Queue.Empty: ## dummy
+			args = self.path
+		arglist = cgi.parse_qs(args)
+		try:
+			s = urllib.unquote(arglist['i'][0])
+			sessionlist.add(s, arglist['t'][0], arglist['h'][0], arglist['p'][0])
+			sitem = sessionlist.get(s)
+		except KeyError:
+			s = None
+			sitem = None
+
+		if sitem and sitem.work:
+			try:
+				try:
+					mydata = urllib.unquote(arglist['d'][0])
+					print 'rcv: ', mydata.strip()
+					sitem.q.qout.put(mydata)
+				except KeyError:
+					item = None
+				try:
+					item = sitem.q.qin.get(True, QUEUE_TIMEOUT)
+				except AttributeError:
+					item = None
+					
+				if not item:
+					sessionlist.rm(s)
+			except (Queue.Empty, ):
+				item = None
+				
+			self.send_response(200)
+			self.end_headers()
+			
+			if item:
+				print 'snd: ' , item.strip()
+				self.wfile.write(item)
+		else:
+			self.send_response(404)
+			self.end_headers()
+
+class SecureHTTPRequestHandler(myHTTPRequestHandler):
+    def setup(self):
+        self.connection = self.request
+        self.rfile = socket._fileobject(self.request, "rb", self.rbufsize)
+        self.wfile = socket._fileobject(self.request, "wb", self.wbufsize)
+
+class httpListener:
+	p = 80
+	r = '/'
+	s = False
+	HandlerClass = myHTTPRequestHandler
+	ServerClass = HTTPServer
+	httpd = None
+	def __init__(self, port, root, ssl):
+		self.p = port
+		self.r = root
+		self.s = ssl
+		if self.s:
+			self.HandlerClass = SecureHTTPRequestHandler
+			self.ServerClass = SecureHTTPServer
+		self.httpd = self.ServerClass(('', self.p), self.HandlerClass)
+	
+	def listen(self):
+		self.httpd.serve_forever()
+
+def main():
+	usage = "usage: %prog [options]"
+	parser = OptionParser(usage=usage)
+	#	parser.add_option('-q', '--quiet', action="store_const", const=0, dest="v", default=1, help='quiet')
+	#	parser.add_option('-v', '--verbose', action="store_const", const=1, dest="v", help='verbose')
+	
+	#parser.add_option('--root', action="store", dest="root", help='rootpath', default='/')
+	parser.add_option('-s', '--ssl', action='store_true', dest='ssl', default=False, help='use https')
+	parser.add_option('-n', '--no-ssl', action='store_false', dest='ssl', help='do not use https (default)')
+	parser.add_option('-p', '--port', action='store', dest='port', type='int', default=80, help='port to listen')
+	parser.add_option('-c', '--cert', action='store', dest='cert', default='cert.pem', help='certificate to use')
+	parser.add_option('-k', '--key', action='store', dest='key', default='key.pem', help='key to use')
+	
+	
+	(options, args) = parser.parse_args()
+	
+	print 'start..'
+	
+	q = queues()
+	
+	if options.ssl:
+		from OpenSSL import SSL
+	global keyfile, certfile
+	keyfile = options.key
+	certfile = options.cert
+	
+	hl = httpListener(options.port, '', options.ssl)
+	hl.listen()
+	
+	sys.stdin.readline()
+	
+	print 'end..'
+
+main()
