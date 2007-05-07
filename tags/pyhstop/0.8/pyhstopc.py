642d48dcb7ef99b148288bb1becc94ec612dab72
flx <flx@b2fd3293-f12a-0410-953c-97324cacd33b>
2007-05-07 10:14:56 +0000

    release 0.8

--- tags/pyhstop/0.8/pyhstopc.py
+++ tags/pyhstop/0.8/pyhstopc.py
@@ -36,7 +36,7 @@ import pycurl
 import signal
 
 #VERSION = pyhstop_common.VERSION
-VERSION = 'HEAD'
+VERSION = '0.8'
 
 QUEUE_TIMEOUT = 10
 DEFAULT_LISTENPORT = 9099
@@ -335,8 +335,8 @@ class tunnelClient:
 			self.cGET.setopt(self.cGET.USERAGENT, options.agent)
 			self.cPUT.setopt(self.cPUT.USERAGENT, options.agent)
 		if self.head:
-			self.cGET.setopt(self.cGET.HTTPHEADER, self.head._make_headers())
-			self.cPUT.setopt(self.cPUT.HTTPHEADER, self.head._make_headers())
+			self.cGET.setopt(self.cGET.HTTP0.8ER, self.head._make_headers())
+			self.cPUT.setopt(self.cPUT.HTTP0.8ER, self.head._make_headers())
 	
 	def newSID(self):
 		self.sid = myHash(time.time())
