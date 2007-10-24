2a166d120bffe6858d88137ff316192c86da5500
flx <flx@b2fd3293-f12a-0410-953c-97324cacd33b>
2007-10-24 11:11:02 +0000

    jhstop2 init part2

---
+++ trunk/jhstop2/src/de/berlios/hstop/jhstop2/jhstopc.java
@@ -0,0 +1,18 @@
+package de.berlios.hstop.jhstop2;
+
+/**
+ * runs the hstop client.
+ * @author flx
+ *
+ */
+public class jhstopc {
+
+	/**
+	 * @param args args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/jhstop2/src/de/berlios/hstop/jhstop2/jhstopd.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.berlios.hstop.jhstop2;
+
+/**
+ * runs the hstop daemon.
+ * @author flx
+ */
+public class jhstopd {
+
+	/**
+	 * @param args args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
